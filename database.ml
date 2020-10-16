638debbae6606c05c074a5ae9d9266d3ebf7b468
ai93 <ai93@cornell.edu>
2020-10-16 17:56:12 -0400

    search in progress

--- database.ml
+++ database.ml
@@ -6,5 +6,15 @@ type task = {id : int; assignee : string; title : string; description : string}
 
 type team = {team_name : string; members : string list}
 
-let get_data =
-  failwith "todo!"
+type task_search_result = 
+  | Success of task
+  | Unsuccessful of string
+
+let get_task_data filename search in_descr=
+  let channel = open_in filename in
+  let rec parse_line chnl search = 
+    match input_line chnl with 
+    | x -> failwith ""
+    | exception (End_of_file) -> Unsuccessful 
+                                   ("Unable to find anything that matches: " ^ 
+                                    search) in failwith ""
