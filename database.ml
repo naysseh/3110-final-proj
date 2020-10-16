26a5a7d69d287fd419075caf840388f404ca356b
ai93 <ai93@cornell.edu>
2020-10-16 12:35:21 -0400

    revision

--- database.ml
+++ database.ml
@@ -1,10 +1,10 @@
 (*Implementation of the data file management*)
 
-let get_data =
-  failwith "todo!"
-
 (*These data types are just drafts to implement basic funcitonality, 
   change as you will*)
 type task = {id : int; assignee : string; title : string; description : string}
 
 type team = {team_name : string; members : string list}
+
+let get_data =
+  failwith "todo!"
