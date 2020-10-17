c50e636d8ea7d86777ac5be321049c4c5f6a9879
Brady Sites <bas339@cornell.edu>
2020-10-17 18:38:31 -0400

    Update test file

--- database_test.ml
+++ database_test.ml
@@ -1,1 +1,24 @@
 open OUnit2
+open Database
+
+(********************************************************************
+   Here, you can put helper functions to test our modules. 
+ ********************************************************************)
+
+
+
+(********************************************************************
+   End helper functions.
+ ********************************************************************)
+
+let database_tests =
+  [
+    (* add tests here *)
+  ]
+
+let suite =
+  "test suite for MS1"  >::: List.flatten [
+    database_tests;
+  ]
+
+let _ = run_test_tt_main suite
