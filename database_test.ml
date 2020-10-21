a4ebf0141fb2c85c57eb507bc1b92c45dfd38744
ai93 <ai93@cornell.edu>
2020-10-20 23:38:45 -0400

    slight test revision

--- database_test.ml
+++ database_test.ml
@@ -16,14 +16,14 @@ let search_tasks_test (name : string) (criteria : string)
 let database_tests =
   [
     search_tasks_test "search for Andrii" "Andrii" 
-      [{id = 3; assignee = "Andrii"; title = "Yeet"; status = "Done";
+      [{id = 1; assignee = "Andrii"; title = "Yeet"; status = "Done";
         description = "\"yeet yote yeeten\""}];
     search_tasks_test "search for o" "o" 
-      [{id = 1; assignee = "Brady"; title = "Code"; status = "To do";
+      [{id = 3; assignee = "Brady"; title = "Code"; status = "To do";
         description = "\"brady just wants to code\""};
        {id = 2; assignee = "Natasha"; title = "Sleep"; status = "Active";
         description = "\"natasha is tired after 3110 and just wants to sleep\""};
-       {id = 3; assignee = "Andrii"; title = "Yeet"; status = "Done";
+       {id = 1; assignee = "Andrii"; title = "Yeet"; status = "Done";
         description = "\"yeet yote yeeten\""}];
     search_tasks_test "search for Nat" "Nat" 
       [{id = 2; assignee = "Natasha"; title = "Sleep"; status = "Active";
