a1ee1f530ffe6833fa935c12b14bf7145f409b04
ai93 <ai93@cornell.edu>
2020-10-20 20:17:55 -0400

    tests

--- database_test.ml
+++ database_test.ml
@@ -4,8 +4,10 @@ open Database
 (********************************************************************
    Here, you can put helper functions to test our modules. 
  ********************************************************************)
-
-
+let search_tasks_test (name : string) (criteria : string) 
+    (expected_output) = 
+  name >:: (fun _ -> 
+      assert_equal expected_output (search_tasks criteria))
 
 (********************************************************************
    End helper functions.
@@ -13,7 +15,19 @@ open Database
 
 let database_tests =
   [
-    (* add tests here *)
+    search_tasks_test "search for Andrii" "Andrii" 
+      [{id = 3; assignee = "Andrii"; title = "Yeet"; status = "Done";
+        description = "\"yeet yote yeeten\""}];
+    search_tasks_test "search for o" "o" 
+      [{id = 1; assignee = "Brady"; title = "Code"; status = "To do";
+        description = "\"brady just wants to code\""};
+       {id = 2; assignee = "Natasha"; title = "Sleep"; status = "Active";
+        description = "\"natasha is tired after 3110 and just wants to sleep\""};
+       {id = 3; assignee = "Andrii"; title = "Yeet"; status = "Done";
+        description = "\"yeet yote yeeten\""}];
+    search_tasks_test "search for Nat" "Nat" 
+      [{id = 2; assignee = "Natasha"; title = "Sleep"; status = "Active";
+        description = "\"natasha is tired after 3110 and just wants to sleep\""}];
   ]
 
 let suite =
